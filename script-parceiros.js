3ff25d3e51c091a80c74d0ff433a28cc9222f514
nicolecarrillo <nicolefcarrillo@gmail.com>
2021-11-15 18:46:13 -0300

    committ

--- script-parceiros.js
+++ script-parceiros.js
@@ -9,6 +9,9 @@ document.addEventListener('mouseup', function(e) {
   }
 });
 
+
+
+
 function abrirOrigi() {
   document.getElementById('origiinfo').style.display = "block";
 }
@@ -21,12 +24,72 @@ document.addEventListener('mouseup', function(e) {
 });
 
 
+
+
 function abrirPic() {
   document.getElementById('picinfo').style.display = "block";
 }
 
 document.addEventListener('mouseup', function(e) {
   let container = document.getElementById('picinfo');
+  if (!container.contains(e.target)) {
+    container.style.display = 'none';
+  }
+});
+
+
+
+
+
+function abrirFlora() {
+  document.getElementById('florainfo').style.display = "block";
+}
+
+document.addEventListener('mouseup', function(e) {
+  let container = document.getElementById('florainfo');
+  if (!container.contains(e.target)) {
+    container.style.display = 'none';
+  }
+});
+
+
+
+
+
+function abrirRural() {
+  document.getElementById('ruralinfo').style.display = "block";
+}
+
+document.addEventListener('mouseup', function(e) {
+  let container = document.getElementById('ruralinfo');
+  if (!container.contains(e.target)) {
+    container.style.display = 'none';
+  }
+});
+
+
+
+
+
+function abrirAmbar() {
+  document.getElementById('ambarinfo').style.display = "block";
+}
+
+document.addEventListener('mouseup', function(e) {
+  let container = document.getElementById('ambarinfo');
+  if (!container.contains(e.target)) {
+    container.style.display = 'none';
+  }
+});
+
+
+
+function abrirEldorado() {
+  document.getElementById('eldoradoinfo').style.display = "block";
+}
+
+document.addEventListener('mouseup', function(e) {
+  let container = document.getElementById('eldoradoinfo');
   if (!container.contains(e.target)) {
     container.style.display = 'none';
   }
