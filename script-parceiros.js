a82faf66104e8544e6c4adbf8c0e15042f62173a
nicolecarrillo <nicolefcarrillo@gmail.com>
2021-11-15 15:36:50 -0300

    commit eba

--- script-parceiros.js
+++ script-parceiros.js
@@ -1,9 +1,33 @@
 function abrirJbs() {
-    var x = document.getElementsByClassName("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+  document.getElementById('jbsinfo').style.display = "block";
 }
 
+document.addEventListener('mouseup', function(e) {
+  let container = document.getElementById('jbsinfo');
+  if (!container.contains(e.target)) {
+    container.style.display = 'none';
+  }
+});
+
+function abrirOrigi() {
+  document.getElementById('origiinfo').style.display = "block";
+}
+
+document.addEventListener('mouseup', function(e) {
+  let container = document.getElementById('origiinfo');
+  if (!container.contains(e.target)) {
+    container.style.display = 'none';
+  }
+});
+
+
+function abrirPic() {
+  document.getElementById('picinfo').style.display = "block";
+}
+
+document.addEventListener('mouseup', function(e) {
+  let container = document.getElementById('picinfo');
+  if (!container.contains(e.target)) {
+    container.style.display = 'none';
+  }
+});
